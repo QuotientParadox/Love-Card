a5764fdca67b539971a52abcd5b0bbd7842d1310
QuotientParadox <117741113+QuotientParadox@users.noreply.github.com>
2022-12-07 13:50:33 -0500

    Made the days before new year counter work

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ function calculate_time()
 {
     let new_year = new Date("january 1, "+new Date().getFullYear());
     let end_year = new Date("december 31, "+new Date().getFullYear());
+    var years_before = Math.floor(Math.abs(end_year.getTime()-new Date())/86400000);
     //console.log("The day's before new year are - "+new_year.getTime()-end_year.getTime());
     //console.log("The day's before new year are - "+Math.abs(new_year.getTime()-end_year.getTime())/5184000000);
 
     //note: please make it so the end_year variable will be subtracted by the todays date
-    console.log("The day's before new year are - "+Math.floor(Math.abs(new_year.getTime()-new Date())/5,184,000,000));
+    //console.log("The day's before new year are - "+Math.floor(Math.abs(new_year.getTime()-new Date())/5,184,000,000));
+    console.log("The day's before new year are - "+years_before);
+    document.getElementById("daysleft").innerHTML = "Days Until New Year - "+years_before;
 }
